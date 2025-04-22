a6181bf59ad7b479f2713072c48bcf99a2171f4f
Paul Schifferer <paul@schifferers.net>
2025-04-21 19:22:44 -0700

    Remove incomplete call

--- data/volume_test.go
+++ data/volume_test.go
@@ -17,7 +17,7 @@ func (suite *DataTestSuite) SetupTest() {
 func (suite *DataTestSuite) TestGetVolume() {
 	logging.Init()
 
-	volume, err := GetVolume(suite.T().Context(), "60c72b2f9b1d4c001c8e4a5f")
+	// volume, err := GetVolume(suite.T().Context(), "60c72b2f9b1d4c001c8e4a5f")
 }
 
 func TestDbTestSuite(t *testing.T) {
